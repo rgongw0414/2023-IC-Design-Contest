d0a3c6e857b554dbd4d67896b82bb60c8b0f1693
rgongw lab <smileeric0414@gmail.com>
2023-05-23 21:48:57 +0800

    step two completed

--- main.cpp
+++ main.cpp
@@ -85,6 +85,59 @@ int main() {
             cout << list[k].first << ", " << list[k].second << endl;
     }
     cout << endl;
-    cout << x1 << ", " << y1 << endl;
-    cout << "max #coverage of circle_1: " << max << endl;
+    cout << "circle_1: (" << x1 << ", " << y1 << ")" << endl;
+    cout << "max #coverage of circle_1: " << std::dec << max << endl;
+    cout << endl;
+
+    // Second step: fix circle_1, find the best central point for circle_2
+    float T0 = 1; // T0 = 5
+    float T = T0;
+    int n = 1, x2 = 15, y2 = 15;
+    //max = 0;
+    int max_tmp = max;
+    vector<bool> detected_2;
+    // detected.assign(40, false);
+    vector<bool> detected_2_tmp;
+    for (int i = 15; i >= 0; i--) {
+        for (int j = 15; j >= 0; j--) {
+            detected_2_tmp.assign(40, false);
+            int cnt = max_tmp;
+            for (int k = 0; k < 40; k++) {
+                // check whether the points are in the radius of 4 of the circle
+                x = list[k].first; y = list[k].second;
+                int dist = (x-i)*(x-i) + (y-j)*(y-j);
+                if (dist < 16 || dist == 16) {
+                    detected_2_tmp[k] = true;
+                    if (!detected[k])
+                        cnt++;
+                }
+            }
+            if (cnt > max) {                
+                max = cnt;
+                x2 = i; y2 = j;
+                detected_2.assign(40, false);
+                for (int k = 0; k < 40; k++) 
+                    detected_2[k] = detected_2_tmp[k];
+            }
+        }
+    }
+    cout << "detected points: \n" << std::hex;
+    for (int k = 0; k < 40; k++) {
+        if (detected_2[k])
+            cout << list[k].first << ", " << list[k].second << endl;
+    }
+    cout << endl;
+    cout << "circle_2: (" << x2 << ", " << y2 << ")" << endl;
+    cout << "max #coverage of circle_1: " << std::dec << max << endl;
+    cout << endl;
+
+
+    
+    // while (T > 0.1) {
+        
+
+
+    //     cout << "n: " << n << ", T: " << T << endl;
+    //     T = T0 * pow(0.9, n++); // Tn = r^n * T0, where T0 = 5
+    // }
 }
