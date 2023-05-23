7501a1addbbe10b700dcae697363756595d0ab95
rgongw lab <smileeric0414@gmail.com>
2023-05-23 22:35:11 +0800

    sth went wrong

--- main.cpp
+++ main.cpp
@@ -51,6 +51,8 @@ int main() {
     // for (auto &elem: list) {
     //     cout << elem.first << ", " << elem.second << endl;
     // }
+
+
     // First step: find the max coverage of the first circle
     int max = 0;
     int x1 = 0, y1 = 0;
@@ -89,15 +91,17 @@ int main() {
     cout << "max #coverage of circle_1: " << std::dec << max << endl;
     cout << endl;
 
-    // Second step: fix circle_1, find the best central point for circle_2
+    
     float T0 = 1; // T0 = 5
     float T = T0;
     int n = 1, x2 = 15, y2 = 15;
-    //max = 0;
     int max_tmp = max;
     vector<bool> detected_2;
-    // detected.assign(40, false);
     vector<bool> detected_2_tmp;
+
+    // Second step: fix circle_1, find the best central point for circle_2        
+    cout << "\n-\nRound_" << n << ":\n\n";
+
     for (int i = 15; i >= 0; i--) {
         for (int j = 15; j >= 0; j--) {
             detected_2_tmp.assign(40, false);
@@ -128,13 +132,113 @@ int main() {
     }
     cout << endl;
     cout << "circle_2: (" << x2 << ", " << y2 << ")" << endl;
-    cout << "max #coverage of circle_1: " << std::dec << max << endl;
+    cout << "max #coverage: " << std::dec << max << endl;
     cout << endl;
 
+    // Third step: fix circle_2, find the best covering central point for circle_2
+    // for (int i = 0; i < 16; i++) {
+    //     for (int j = 15; j >= 0; j--) {
+    //         detected_tmp.assign(40, false);
+    //         int cnt = 0;
+    //         for (int k = 0; k < 40; k++) {
+    //             // check whether the points are in the radius of 4 of the circle
+    //             x = list[k].first; y = list[k].second;
+    //             int dist = (x-i)*(x-i) + (y-j)*(y-j);
+    //             if (dist < 16 || dist == 16) {
+    //                 detected_tmp[k] = true;
+    //                 if (!detected_2[k])
+    //                     cnt++;
+    //             }
+    //         }
+    //         if (cnt > max) {                
+    //             max = cnt;
+    //             x1 = i; y1 = j;
+    //             detected.assign(40, false);
+    //             for (int k = 0; k < 40; k++) 
+    //                 detected[k] = detected_tmp[k];
+    //         }
+    //     }
+    // }
+    // cout << "detected points: \n";
+    // for (int k = 0; k < 40; k++) {
+    //     if (detected[k])
+    //         cout << list[k].first << ", " << list[k].second << endl;
+    // }
+    // cout << endl;
+    // cout << "circle_1: (" << std::hex << x1 << ", " << y1 << ")" << endl;
+    // cout << "max #coverage: " << std::dec << max << endl;
+    // cout << endl;   
 
-    
     // while (T > 0.1) {
-        
+    //     // Second step: fix circle_1, find the best central point for circle_2        
+    //     cout << "\n-\nRound_" << n << ":\n\n";
+
+    //     for (int i = 15; i >= 0; i--) {
+    //         for (int j = 15; j >= 0; j--) {
+    //             detected_2_tmp.assign(40, false);
+    //             int cnt = 0;
+    //             for (int k = 0; k < 40; k++) {
+    //                 // check whether the points are in the radius of 4 of the circle
+    //                 x = list[k].first; y = list[k].second;
+    //                 int dist = (x-i)*(x-i) + (y-j)*(y-j);
+    //                 if (dist < 16 || dist == 16) {
+    //                     detected_2_tmp[k] = true;
+    //                     if (!detected[k])
+    //                         cnt++;
+    //                 }
+    //             }
+    //             if (cnt > max) {                
+    //                 max = cnt;
+    //                 x2 = i; y2 = j;
+    //                 detected_2.assign(40, false);
+    //                 for (int k = 0; k < 40; k++) 
+    //                     detected_2[k] = detected_2_tmp[k];
+    //             }
+    //         }
+    //     }
+    //     cout << "detected points: \n" << std::hex;
+    //     for (int k = 0; k < 40; k++) {
+    //         if (detected_2[k])
+    //             cout << list[k].first << ", " << list[k].second << endl;
+    //     }
+    //     cout << endl;
+    //     cout << "circle_2: (" << x2 << ", " << y2 << ")" << endl;
+    //     cout << "max #coverage: " << std::dec << max << endl;
+    //     cout << endl;
+
+    //     // Third step: fix circle_2, find the best covering central point for circle_2
+    //     for (int i = 0; i < 16; i++) {
+    //         for (int j = 15; j >= 0; j--) {
+    //             detected_tmp.assign(40, false);
+    //             int cnt = 0;
+    //             for (int k = 0; k < 40; k++) {
+    //                 // check whether the points are in the radius of 4 of the circle
+    //                 x = list[k].first; y = list[k].second;
+    //                 int dist = (x-i)*(x-i) + (y-j)*(y-j);
+    //                 if (dist < 16 || dist == 16) {
+    //                     detected_tmp[k] = true;
+    //                     if (!detected_2[k])
+    //                         cnt++;
+    //                 }
+    //             }
+    //             if (cnt > max) {                
+    //                 max = cnt;
+    //                 x1 = i; y1 = j;
+    //                 detected.assign(40, false);
+    //                 for (int k = 0; k < 40; k++) 
+    //                     detected[k] = detected_tmp[k];
+    //             }
+    //         }
+    //     }
+    //     cout << "detected points: \n";
+    //     for (int k = 0; k < 40; k++) {
+    //         if (detected[k])
+    //             cout << list[k].first << ", " << list[k].second << endl;
+    //     }
+    //     cout << endl;
+    //     cout << "circle_1: (" << std::hex << x1 << ", " << y1 << ")" << endl;
+    //     cout << "max #coverage: " << std::dec << max << endl;
+    //     cout << endl;   
 
 
     //     cout << "n: " << n << ", T: " << T << endl;
