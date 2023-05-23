3001807736230b5db1a4997154e01de4523ec654
rgongw lab <smileeric0414@gmail.com>
2023-05-23 19:53:11 +0800

    complete first step

--- main.cpp
+++ main.cpp
@@ -31,10 +31,11 @@ int main() {
     // cout << sol << endl;
 
     vector<vector<int>> map(16, vector<int>(16, 0));
-    // int map[16][16];
+    vector<pair<int, int>> list;
     int x, y;
     while (cin >> x >> y) {
         map[x][y] = 1;
+        list.eb(make_pair(x, y));
         // cout << x << ", " << y << endl;
     }
     int tmp = 0;
@@ -46,4 +47,44 @@ int main() {
         for (auto &elem: row) cout << elem << " ";
         cout << endl;
     }
+
+    // for (auto &elem: list) {
+    //     cout << elem.first << ", " << elem.second << endl;
+    // }
+    // First step: find the max coverage of the first circle
+    int max = 0;
+    int x1 = 0, y1 = 0;
+    vector<bool> detected;
+    // detected.assign(40, false);
+    vector<bool> detected_tmp;
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            detected_tmp.assign(40, false);
+            int cnt = 0;
+            for (int k = 0; k < 40; k++) {
+                // check whether the points are in the radius of 4 of the circle
+                x = list[k].first; y = list[k].second;
+                int dist = (x-i)*(x-i) + (y-j)*(y-j);
+                if (dist < 16 || dist == 16) {
+                    detected_tmp[k] = true;
+                    cnt++;
+                }
+            }
+            if (cnt > max) {                
+                max = cnt;
+                x1 = i; y1 = j;
+                detected.assign(40, false);
+                for (int k = 0; k < 40; k++) 
+                    detected[k] = detected_tmp[k];
+            }
+        }
+    }
+    cout << "detected points: \n";
+    for (int k = 0; k < 40; k++) {
+        if (detected[k])
+            cout << list[k].first << ", " << list[k].second << endl;
+    }
+    cout << endl;
+    cout << x1 << ", " << y1 << endl;
+    cout << "max #coverage of circle_1: " << max << endl;
 }
