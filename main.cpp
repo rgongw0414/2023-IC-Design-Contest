a4e6056a68b105f6f61f4a0e1bd7e9e37d8e2c11
rgongw lab <smileeric0414@gmail.com>
2023-05-24 16:05:56 +0800

    TODO: apply weight for overlapping

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ int main() {
     cin >> sol_tmp;
     cout << sol_tmp << endl;
     int sol = (sol_tmp[8] - '0')*10 + (sol_tmp[9] - '0');
-    // cout << sol << endl;
 
     vector<vector<int>> map(16, vector<int>(16, 0));
     vector<pair<int, int>> list;
@@ -26,7 +25,6 @@ int main() {
     while (cin >> x >> y) {
         map[x][y] = 1;
         list.eb(make_pair(x, y));
-        // cout << x << ", " << y << endl;
     }
     int tmp = 0;
     cout << "  ";
@@ -37,11 +35,6 @@ int main() {
         for (auto &elem: row) cout << elem << " ";
         cout << endl;
     }
-
-    // for (auto &elem: list) {
-    //     cout << elem.first << ", " << elem.second << endl;
-    // }
-
 
     // First step: find the max coverage of the first circle
     int max = 0;
@@ -80,7 +73,7 @@ int main() {
     int max1 = max; // max1: the max coverage found of circle_1
     int max2 = 0; // max2: the max coverage found of circle_2
     
-    float T = 1000; // T0 = 10
+    float T = 10; // T0 = 10
     int n = 1, x2 = rand() % 16, y2 = rand() % 16;
     vector<bool> detected_2(40, false);
     vector<bool> detected_2_tmp(40, false);
@@ -89,26 +82,29 @@ int main() {
         cout << "\n-\nRound_" << n++ << ":\n";
 
         // Second step: fix circle_1, find the best central point for circle_2        
-        for (int k = 0; k < 40; k++) detected_2_tmp[k] = false; // avoid segmentation fault
-        int cnt = 0;
+        for (int k = 0; k < 40; k++) detected_2_tmp[k] = false; // reset the tmp; looks stupid, but effectively avoid segmentation fault 
+        
         int x2_tmp = x2 + pow(-1, rand()%2) * (rand()%3); // x2 = x2 +- (1~2)
         if (x2_tmp < 0) x2_tmp = 0;
         else if (x2_tmp > 15) x2_tmp = 15;
         int y2_tmp = y2 + pow(-1, rand()%2) * (rand()%3); // y2 = y2 +- (1~2)
         if (y2_tmp < 0) y2_tmp = 0;
         else if (y2_tmp > 15) y2_tmp = 15;
+
+        int cnt = 0, overlapped_n = 0;
         for (int k = 0; k < 40; k++) {
             // Check whether the points are in the radius of 4 of the circle
             x = list[k].first; y = list[k].second;
             int dist = (x-x2_tmp)*(x-x2_tmp) + (y-y2_tmp)*(y-y2_tmp);
             if (dist < 16 || dist == 16) {
                 detected_2_tmp[k] = true;
-                if (!detected[k]) cnt++;
-                // else cnt--; // punishment for overlapping
+                if (detected[k]) overlapped_n++; // punishment for overlapping
+                cnt++; 
             }
         }
-        if (max1 + cnt > max || max1 + cnt == max) {                
-            max2 = cnt;
+        int cost = max1 + cnt - overlapped_n;
+        if (cost > max || cost == max) {                
+            max2 = cnt - overlapped_n;
             max = max1 + max2;
             x2 = x2_tmp; y2 = y2_tmp;
             for (int k = 0; k < 40; k++) detected_2[k] = detected_2_tmp[k];
@@ -118,7 +114,7 @@ int main() {
             float sample = r / RAND_MAX;
             float prob = exp(-(max-(max1+cnt))/T);                    
             if (sample < prob) { // accept the worse solution
-                max2 = cnt;
+                max2 = cnt - overlapped_n;
                 max = max1 + max2;
                 x2 = x2_tmp; y2 = y2_tmp;
                 for (int k = 0; k < 40; k++) detected_2[k] = detected_2_tmp[k];
@@ -131,25 +127,29 @@ int main() {
 
         // Third step: fix circle_2, find the best covering central point for circle_1
         for (int k = 0; k < 40; k++) detected_tmp[k] = false; // avoid segmentation fault
-        cnt = 0;
+
         int x1_tmp = x1 + pow(-1, rand()%2) * (rand()%3); // x1 = x1 +- (1~2)
         if (x1_tmp < 0) x1_tmp = 0;
         else if (x1_tmp > 15) x1_tmp = 15;
         int y1_tmp = y1 + pow(-1, rand()%2) * (rand()%3); // y2 = y1 +- (1~2)
         if (y1_tmp < 0) y1_tmp = 0;
         else if (y1_tmp > 15) y1_tmp = 15;
+
+        cnt = 0, overlapped_n = 0;
         for (int k = 0; k < 40; k++) {
-            // check whether the points are in the radius of 4 of the circle
+            // Check whether the points are in the radius of 4 of the circle
             x = list[k].first; y = list[k].second;
             int dist = (x-x1_tmp)*(x-x1_tmp) + (y-y1_tmp)*(y-y1_tmp);
             if (dist < 16 || dist == 16) {
                 detected_tmp[k] = true;
-                if (!detected_2[k]) cnt++;
-                // else cnt--; // punishment for overlapping
+                if (detected_2[k]) overlapped_n++; // punishment for overlapping
+                cnt++; 
             }
         }
-        if (max2 + cnt > max || max2 + cnt == max) {                
-            max1 = cnt;
+
+        cost = max2 + cnt - overlapped_n;
+        if (cost > max || cost == max) {                
+            max1 = cnt - overlapped_n;
             max = max1 + max2;
             x1 = x1_tmp; y1 = y1_tmp;
             for (int k = 0; k < 40; k++) detected[k] = detected_tmp[k];
@@ -159,7 +159,7 @@ int main() {
             float sample = r / RAND_MAX;
             float prob = exp(-(max-(max1+cnt))/T);                    
             if (sample < prob) { // accept the worse solution
-                max1 = cnt;
+                max1 = cnt - overlapped_n;
                 max = max1 + max2;
                 x1 = x1_tmp; y1 = y1_tmp;
                 for (int k = 0; k < 40; k++) detected[k] = detected_tmp[k];
@@ -172,7 +172,7 @@ int main() {
         cout << "\tmax #coverage: " << std::dec << max << endl;
         cout << endl;   
 
-        T = T * 0.922; // Tn = r^n * T0, where T0 = 5
+        T = T * 0.8787; // Tn = r^n * T0, where T0 = 5
         cout << "\tT: " << T << endl;
         cout << "\t" << sol_tmp << endl;
         if (max == sol) {
