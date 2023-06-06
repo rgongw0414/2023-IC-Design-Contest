0fe90127496af4b52d6f825d1a96a1ad6a8cb6ae
rgongw lab <smileeric0414@gmail.com>
2023-06-06 21:45:29 +0800

    define parameters

--- LASER.v
+++ LASER.v
@@ -11,39 +11,41 @@ output reg DONE);
 
 reg [3:0]current_state;
 reg [3:0]next_state;
-reg [2:0] interval;
+
 parameter  INIT = 0;
 parameter READ = 1;
-parameter CAL_CIRCLE2_LOCATION = 2;
-parameter CAL_COVER_RATE = 3;
-parameter CAL_UP = 4;
-parameter CAL_DOWN = 5;
-parameter CAL_LEFT = 6;
-parameter CAL_RIGHT = 7;
-parameter OPTIMIZE = 8;
-parameter OUTPUT = 9;
-reg change;
-reg circle;
-reg [5:0] tmp_max_cover;
-reg [5:0] max_cover;
-reg [5:0] counter;
-reg [3:0] bufferX [39:0];
-reg [3:0] bufferY [39:0];
-wire [10:0] add_1;
-wire [10:0] add_2;
-reg [10:0] temp_1;
-reg [10:0] temp_2;
-reg [10:0] temp_3;
-reg [10:0] temp_4;
-reg [3:0] dir1, dir2;
-assign add_1 = (temp_1 + temp_2);
-assign add_2 = (temp_3 + temp_4);
-integer i;
+
+parameter STEP_ONE = 2;
+parameter STEP_TWO = 3;
+parameter STEP_THREE = 4;
+// parameter OPTIMIZE = 8;
+parameter OUTPUT = 5;
+
+reg [5:0] cover_current; // num of current covered points 
+reg [5:0] cover_max;
+reg [5:0] cnt;
+reg [3:0] corX [39:0]; // coordinates array of all input points
+reg [3:0] corY [39:0];
+reg [3:0] tmpX1, tmpY1, tmpX2, tmpY2; // current x and y
+wire [4:0] dx1, dy1, dx2, dy2;
+
+// *** 
+// compute abs value of dist. b/t tmpX/Y and corXY
+assign  dx1 = (tmpX1 > corX[cnt])? tmpX1 - corX[cnt] : corX[cnt] - tmpX1;
+assign  dy1 = (tmpY1 > corY[cnt])? tmpY1 - corY[cnt] : corY[cnt] - tmpY1;
+assign  dx2 = (tmpX2 > corX[cnt])? tmpX2 - corX[cnt] : corX[cnt] - tmpX2;
+assign  dy2 = (tmpY2 > corY[cnt])? tmpY2 - corY[cnt] : corY[cnt] - tmpY2; 
+// ***
+/*
+cover_current <= cover_current + 
+        ((dx1 + dy1 <= 4) || (dx1 == 3 && dy1 == 2) || (dx1 == 2 && dy1 == 3) || 
+        (dx2 + dy2 <= 4) || (dx2 == 3 && dy2 == 2) || (dx2 == 2 && dy2 == 3));
+*/
 
 reg [3:0] tmpx1, tmpx2, tmpy1, tmpy2;
 wire [8:0] mul1, mul2;
-assign mul1 =(tmpx1 - bufferX[counter])*(tmpx1 - bufferX[counter]) + (tmpy1 - bufferY[counter])*(tmpy1 - bufferY[counter]);
-assign mul2 =(tmpx2 - bufferX[counter])*(tmpx2 - bufferX[counter]) + (tmpy2 - bufferY[counter])*(tmpy2 - bufferY[counter]);
+assign mul1 =(tmpx1 - bufferX[cnt])*(tmpx1 - bufferX[cnt]) + (tmpy1 - bufferY[cnt])*(tmpy1 - bufferY[cnt]);
+assign mul2 =(tmpx2 - bufferX[cnt])*(tmpx2 - bufferX[cnt]) + (tmpy2 - bufferY[cnt])*(tmpy2 - bufferY[cnt]);
 always @(posedge CLK or posedge RST) begin
     if(RST)
         current_state <= INIT;
@@ -56,20 +58,20 @@ case (current_state)
     INIT:
         next_state =(RST)?INIT:READ;
     READ:
-        next_state =  (counter == 6'd39)?CAL_CIRCLE2_LOCATION:READ;
+        next_state =  (cnt == 6'd39)?CAL_CIRCLE2_LOCATION:READ;
     CAL_CIRCLE2_LOCATION:
         next_state = CAL_COVER_RATE;
     CAL_COVER_RATE:
-        next_state = (counter == 6'd40)?CAL_DOWN:CAL_COVER_RATE;
+        next_state = (cnt == 6'd40)?CAL_DOWN:CAL_COVER_RATE;
     CAL_DOWN:
-        next_state =(counter == 6'd40)?CAL_UP:CAL_DOWN;
+        next_state =(cnt == 6'd40)?CAL_UP:CAL_DOWN;
     CAL_UP:
-        next_state =(counter == 6'd40)?CAL_RIGHT:CAL_UP;
+        next_state =(cnt == 6'd40)?CAL_RIGHT:CAL_UP;
     
     CAL_RIGHT:
-        next_state =(counter == 6'd40)?CAL_LEFT:CAL_RIGHT;
+        next_state =(cnt == 6'd40)?CAL_LEFT:CAL_RIGHT;
     CAL_LEFT:
-        next_state =(counter == 6'd40)?OPTIMIZE:CAL_LEFT;
+        next_state =(cnt == 6'd40)?OPTIMIZE:CAL_LEFT;
         
     OPTIMIZE:
         next_state =(dir1 == 0 && dir2 == 0 && change ==0 && interval == 3'd7)?OUTPUT:CAL_DOWN;
@@ -79,90 +81,49 @@ case (current_state)
         next_state = INIT;
 endcase
 end
-// buffer
-always @(posedge CLK)
-begin
-    if(RST)
-    begin
-        for(i=0;i<40;i=i+1)
-        begin
-            bufferX[i] <= 4'b0;
-            bufferY[i] <= 4'b0;
-        end
-        temp_1 <= 0;
-        temp_2 <= 0;
-        temp_3 <= 0;
-        temp_4 <= 0;
-    end
-    else if(next_state == READ || current_state == READ)
-    begin
-        if(counter == 0)
-        begin
-            bufferX[counter] <= X;
-            bufferY[counter] <= Y;
-            temp_1 <= X;
-            temp_3 <= Y;
-        end
-        else if(counter == 1)
-        begin
-            bufferX[counter] <= X;
-            bufferY[counter] <= Y;
-            temp_2 <= X;
-            temp_4 <= Y;
-        end
-        else
-        begin
-            bufferX[counter] <= X;
-            bufferY[counter] <= Y;
-            temp_1 <= add_1;
-            temp_3 <= add_2;
-            temp_2 <= X;
-            temp_4 <= Y;
-        end
-        
-    end
 
-end
-// tmp_max_cover
+// cover_current
 always @(posedge CLK) begin
 if(RST)
-    tmp_max_cover <= 6'd0;
+    cover_current <= 6'd0;
 else if(current_state == CAL_COVER_RATE)
 begin
-    if(counter >0)
+    if(cnt >0)
     begin
         if(mul1 <=16)
-            tmp_max_cover <= tmp_max_cover + 1;
+            cover_current <= cover_current + 1;
     end
 end
 else if(current_state == CAL_UP || current_state == CAL_DOWN || current_state == CAL_LEFT ||current_state == CAL_RIGHT)
 begin
-    if(counter >0)
+    if(cnt >0)
     begin
         if(mul1 <=16 || mul2 <= 16)
-            tmp_max_cover <= tmp_max_cover + 1;
+            cover_current <= cover_current + 1;
     end
     else
-        tmp_max_cover <= 0;
+        cover_current <= 0;
 end
 else if(current_state == OUTPUT)
-    tmp_max_cover <= 0;
+    cover_current <= 0;
 end
-// max_cover
+
+// cover_max
 always @(posedge CLK) begin
 if(RST)
-    max_cover <= 6'd0;
+    cover_max <= 6'd0;
 else if(current_state == CAL_COVER_RATE || current_state == CAL_UP || current_state == CAL_DOWN || current_state == CAL_LEFT || current_state == CAL_RIGHT)
 begin
-    if(counter == 6'd40)
+    if(cnt == 6'd40)
     begin
-        if(tmp_max_cover >max_cover)
-            max_cover <= tmp_max_cover;
+        if(cover_current >cover_max)
+            cover_max <= cover_current;
     end
 end
 else if(current_state == OUTPUT)
-    max_cover <= 0;
+    cover_max <= 0;
 end
+
 // dir
 always @(posedge CLK)
 begin
@@ -183,7 +144,7 @@ begin
     dir1 <= 0;
     dir2 <= 0;
 end
-else if(counter == 6'd40 && tmp_max_cover >max_cover)
+else if(cnt == 6'd40 && cover_current >cover_max)
 begin
     if(circle)
     begin
@@ -228,125 +189,41 @@ if(RST)
     circle <= 0;
 else if(current_state == CAL_LEFT)
 begin
-    if(counter == 6'd40)
+    if(cnt == 6'd40)
         if((!circle && dir2==0) || (circle && dir1 == 0))
             circle <= ~circle;
 end
 else if(current_state == OUTPUT)
     circle <= 0;
 end
-// tmpx. tmpy
+
+
+// cnt, or Temperature for 15 ~ 25 round
 always @(posedge CLK) begin
 if(RST)
-begin
-    tmpx1 <= 0;
-    tmpx2 <= 0;
-    tmpy1 <= 0;
-    tmpy2 <= 0;
-end
-else if(next_state == CAL_COVER_RATE)
-begin
-    tmpx1 <= C1X;
-    tmpy1 <= C1Y;
-end
-else if(current_state == CAL_UP)
-begin
-    if(circle)//circel 1
-    begin
-        tmpx1 <= C1X-interval;
-        tmpy1 <= C1Y;
-        tmpx2 <= C2X;
-        tmpy2 <= C2Y;
-    end
-    else // circle 2
-    begin
-        tmpx1 <= C1X;
-        tmpy1 <= C1Y;
-        tmpx2 <= C2X-interval;
-        tmpy2 <= C2Y;
-    end
-end
-else if(current_state == CAL_DOWN)
-begin
-    if(circle)//circel 1
-    begin
-        tmpx1 <= C1X+interval;
-        tmpy1 <= C1Y;
-        tmpx2 <= C2X;
-        tmpy2 <= C2Y;
-    end
-    else // circle 2
-    begin
-        tmpx1 <= C1X;
-        tmpy1 <= C1Y;
-        tmpx2 <= C2X+interval;
-        tmpy2 <= C2Y;
-    end
-end
-else if(current_state == CAL_RIGHT)
-begin
-    if(circle)//circel 1
-    begin
-        tmpx1 <= C1X;
-        tmpy1 <= C1Y-interval;
-        tmpx2 <= C2X;
-        tmpy2 <= C2Y;
-    end
-    else // circle 2
-    begin
-        tmpx1 <= C1X;
-        tmpy1 <= C1Y;
-        tmpx2 <= C2X;
-        tmpy2 <= C2Y+interval;
-    end
-end
-else if(current_state == CAL_LEFT)
-begin
-    if(circle)//circle 2
-    begin
-        tmpx1 <= C1X;
-        tmpy1 <= C1Y+interval;
-        tmpx2 <= C2X;
-        tmpy2 <= C2Y;
-    end
-    else // circle 2
-    begin
-        tmpx1 <= C1X;
-        tmpy1 <= C1Y;
-        tmpx2 <= C2X;
-        tmpy2 <= C2Y-interval;
-    end
-end
-
-end
-
-
-// counter
-always @(posedge CLK) begin
-if(RST)
-    counter <= 0;
+    cnt <= 0;
 else if(current_state == OUTPUT)
-    counter <= 0;
+    cnt <= 0;
 else if(next_state == READ)
-    counter <= counter + 1;
+    cnt <= cnt + 1;
 else if(current_state == CAL_CIRCLE2_LOCATION)
-    counter <= 0;
+    cnt <= 0;
 else if(current_state == CAL_COVER_RATE)
 begin
-    if(counter == 40)
-        counter <= 0;
+    if(cnt == 40)
+        cnt <= 0;
     else
-        counter <= counter + 1;
+        cnt <= cnt + 1;
 end
 else if(current_state == CAL_UP || current_state == CAL_DOWN || current_state == CAL_LEFT|| current_state == CAL_RIGHT )
 begin
-    if(counter ==6'd40)
-        counter <= 0;
+    if(cnt ==6'd40)
+        cnt <= 0;
     else 
-        counter <= counter +1;
+        cnt <= cnt +1;
 end
 else 
-    counter <= 0;
+    cnt <= 0;
 end
 
 // OUTPUT
@@ -367,9 +244,9 @@ begin
 end
 else if(current_state == CAL_UP || current_state == CAL_DOWN || current_state == CAL_LEFT || current_state == CAL_RIGHT)
 begin
-    if(counter == 6'd40)
+    if(cnt == 6'd40)
     begin
-        if(tmp_max_cover > max_cover)
+        if(cover_current > cover_max)
         begin
             C1X <= tmpx1;
             C1Y <= tmpy1;
@@ -390,17 +267,5 @@ always @(posedge CLK) begin
     DONE <= 1'b0;
 end
 
-// interval
-always @(posedge CLK) begin
-if(RST)
-    interval <=1;
-else if(change)
-    interval <= 1;
-else if(current_state == OPTIMIZE && change == 0 && dir1 == 0 && dir2 ==0)
-    interval <= interval +1;
-else if(current_state == READ)
-    interval <= 1;
-
-end
 endmodule
 
---
+++ xcelium.d/run.lnx8664.22.03.d/.timestamp.ts
@@ -0,0 +1,3 @@
+1089862444 /home/eda/CBDK018_TSMC_Artisan/CIC/Verilog/tsmc18_neg.v
+1686054988 /home/eda/2023-IC-Design-Contest/tb.sv
+1686055502 /home/eda/2023-IC-Design-Contest/LASER_syn.v
